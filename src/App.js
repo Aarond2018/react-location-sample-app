dd5c6b92fdfec6008b3b96625cbc587fe1149048
Aarond2018 <aarondamilola1998@gmail.com>
2021-12-12 23:52:36 -0800

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+/* import {Routes, Route, Link} from 'react-router-dom' */
+import { Router, Outlet, ReactLocation, Link } from "react-location";
+import Home from "./Home";
+import Contact from "./Contact";
+import Info from "./Info";
+import "./App.css";
+
+const routes = [
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/contact",
+		element: <Contact />,
+	},
+	{
+		path: "/info",
+		element: <Info />,
+	},
+];
+
+const location = new ReactLocation();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/info" element={<Info/>}></Route>
+      </Routes> */}
+
+			<Router routes={routes} location={location}>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
+					<li>
+						<Link to="/info">Info</Link>
+					</li>
+				</ul>
+				<Outlet />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
